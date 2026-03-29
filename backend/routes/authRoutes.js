4292e15a5bbab40e7843601ca745b79c4c54f645
NatiqueAsghar <mdnatiqueasghar@gmail.com>
2026-03-29 22:34:01 +0530

    Initial commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import register from '../controllers/authControllers.js'
+import {register, login, logout} from '../controllers/authControllers.js'
+
+
 
 const authRoutes = express.Router()
 
 authRoutes.post('/register', register)
+authRoutes.post('/login', login)
+authRoutes.post('/logout', logout)
 
 export default authRoutes
