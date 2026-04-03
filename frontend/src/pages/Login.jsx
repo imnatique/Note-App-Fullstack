3b596f6a17d2eecf1897dbc0a3759a50203b4ee7
NatiqueAsghar <mdnatiqueasghar@gmail.com>
2026-04-04 00:13:37 +0530

    Initial commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,17 +1,50 @@
-import React from "react";
-import {Link} from 'react-router-dom'
-
+import React, { useState } from "react";
+import {Link, useNavigate} from 'react-router-dom'
+import { post } from "../../services/ApiEndPoint";
+import toast from "react-hot-toast";
 function Login() {
+  const navigate = useNavigate()
+  const [value, setValue] = useState({
+    email:"",
+    password:""
+  })
+
+  const handleChange = (e) => {
+    setValue({
+      ...value,
+      [e.target.name]:e.target.value
+    })
+  }
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault()
+    try {
+      const request = await post('auth/login', value)
+      const response = request.data
+      if(response.success) {
+        toast.success(response.message)
+        navigate('/')
+      }
+      console.log('res', response)
+    } catch (error) {
+      if(error.response) {
+        toast.error(error.response.data.message)
+      }
+      console.log(error.response.data)
+    }
+  }
   return (
     <div className="container min-vh-100 d-flex justify-content-center align-items-center">
       <div className="form-container broder shadow p-5 rounded-4 bg-white w-50">
         <h2 className="text-center mb-4 fw-bold">Login</h2>
-        <form className="d-flex flex-column">
+        <form className="d-flex flex-column" onSubmit={handleSubmit}>
           <div className="form-group mb-3">
             <label htmlFor="email" className="form-label">Email</label>
             <input
               type="email"
               name="email"
+              value={value.email}
+              onChange={handleChange}
               className="form-control"
               placeholder="Email"
               aria-label="Email"
@@ -24,6 +57,8 @@ function Login() {
             <input
               type="password"
               name="password"
+              value={value.password}
+              onChange={handleChange}
               className="form-control"
               placeholder="Enter your password"
               id="password"
